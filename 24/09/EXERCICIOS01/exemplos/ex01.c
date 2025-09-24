10eae886043d5ea77ebb823dc97e6cc762207c85
Eberth da Silva Rodrigues <eberthsilva011@gmail.com>
2025-09-24 19:35:22 -0300

    add:
    pastas com os dias e especificação de cada atividade, exercicio e exemplo feito em sala de aula.

---
+++ 24/09/EXERCICIOS01/exemplos/ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+
+char ch;
+ch = getchar();
+if (ch=='p');
+
+printf("Voce digitou a tecla p");
+
+
+getchar();
+return 0;
+
+
+} 
---
+++ 24/09/EXERCICIOS01/exemplos/ex02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	
+	if (getchar()=='p'){
+	printf("Voce digitou p");
+	printf("Precione outra tecla: ");
+	getchar();
+	}
+}
