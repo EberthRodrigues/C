1910ba141d9041de215b98044033be81bd88cb25
Eberth da Silva Rodrigues <eberthsilva011@gmail.com>
2025-10-07 19:10:44 -0300

    Update exe01.c

--- 02-10/Atividade/exe01.c
+++ 02-10/Atividade/exe01.c
@@ -1,5 +1,5 @@
 1 - Faca um programa que, no momento de preecher um vetor com 8 numeros inteiros ja os armazenem de forma crescente (usar vetor e while e for).
-.
+
 #include <stdio.h>
 
 int main() {
