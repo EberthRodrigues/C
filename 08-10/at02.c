3b9dfc9e46b7a026b350f13e2ebad5f3a29c30b2
Eberth da Silva Rodrigues <eberthsilva011@gmail.com>
2025-10-08 19:02:44 -0300

    Update at02.c

---
+++ 08-10/at02.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    char nomesLojas[8][20];
+    char nomesProdutos[4][20];
+    float precos[8][4];
+    int loja, produto;
+
+    // Leitura dos nomes das lojas
+    for (loja = 0; loja < 8; loja++) {
+        printf("Digite o nome da loja %d: ", loja + 1);
+        scanf("%s", nomesLojas[loja]);
+    }
+
+    // Leitura dos nomes dos produtos
+    for (produto = 0; produto < 4; produto++) {
+        printf("Digite o nome do produto %d: ", produto + 1);
+        scanf("%s", nomesProdutos[produto]);
+    }
+
+    // Leitura dos preços
+    for (loja = 0; loja < 8; loja++) {
+        for (produto = 0; produto < 4; produto++) {
+            printf("Digite o preco do produto %s na loja %s: ", nomesProdutos[produto], nomesLojas[loja]);
+            scanf("%f", &precos[loja][produto]);
+        }
+    }
+
+    // Exibir produtos até R$60
+    printf("\nProdutos com preco ate R$60,00:\n");
+    for (loja = 0; loja < 8; loja++) {
+        for (produto = 0; produto < 4; produto++) {
+            if (precos[loja][produto] <= 60)
+                printf("%s - %s: R$%.2f\n", nomesProdutos[produto], nomesLojas[loja], precos[loja][produto]);
+        }
+    }
+
+    return 0;
+}
