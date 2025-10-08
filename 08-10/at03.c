5e07e51733ae35dd25da56ad954cb1ee7c97ddbc
Eberth da Silva Rodrigues <eberthsilva011@gmail.com>
2025-10-08 19:02:57 -0300

    Update at03.c

---
+++ 08-10/at03.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[18];
+    int matriz[3][6];
+    int linha, coluna;
+    int contador = 0;
+
+    // Ler o vetor
+    for (int i = 0; i < 18; i++) {
+        printf("Digite o elemento %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    // Distribuir para a matriz
+    for (linha = 0; linha < 3; linha++) {
+        for (coluna = 0; coluna < 6; coluna++) {
+            matriz[linha][coluna] = vetor[contador];
+            contador++;
+        }
+    }
+
+    // Mostrar matriz
+    printf("\nMatriz 3x6:\n");
+    for (linha = 0; linha < 3; linha++) {
+        for (coluna = 0; coluna < 6; coluna++) {
+            printf("%3d ", matriz[linha][coluna]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
